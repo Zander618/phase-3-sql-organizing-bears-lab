da91896b6186af51f05e9b15206db83759191735
Zachary Sirna <zsirna@yahoo.com>
2022-05-04 21:37:37 -0400

    commit tree b952fcde57ce6ba0bd9ac426c78be40c39bf544a

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr.Chocolate", 5, "M", "brown", "mean", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 10, "M", "black", "nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 8, "F", "white", "nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 3, "F", "brown", "shy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 2, "F", "black", "nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 1, "M", "white", "playful", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 7, "F", "brown", "mean", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 6, "M", "black", "really mean", 1);
